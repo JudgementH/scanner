b28c0d18731f610185498dc9e2d442b38acac847
Judgement_H <446995859@qq.com>
2020-10-14 23:21:25 +0800

    update transform and ocr

---
+++ edge/edge.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2020/10/14 13:11
+
+'利用canny算法提取边缘'
+
+__author__ = 'Judgement'
+
+import cv2
+import numpy as np
+from perspective import transform
+
+
+def getOutline(img_src):
+    # 输入图像路径，返回透视后轮廓的图形数组
+
+    img = cv2.imread(img_src)
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    # 去噪
+    blur = cv2.GaussianBlur(gray, (5, 5), sigmaX=0)
+    # 二值化边缘
+    binary = cv2.Canny(blur, threshold1=30, threshold2=120)
+
+    # 腐蚀
+    erode = cv2.erode(binary, kernel=cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 1)))
+    # 膨胀
+    dilate = cv2.dilate(erode, kernel=cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3)))
+
+    # 轮廓提取
+    contours, hierarchy = cv2.findContours(dilate, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+
+    # 初始轮廓
+    outline = cv2.multiply(img, 0)
+    cv2.drawContours(outline, contours, 0, (255, 255, 255), 2)
+    outline = cv2.cvtColor(outline, cv2.COLOR_RGB2GRAY)
+
+    # 再提取
+    contours, hierarchy = cv2.findContours(outline, mode=cv2.RETR_LIST, method=cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+    outline = cv2.multiply(img, 0)
+    cv2.drawContours(outline, contours, 0, (255, 255, 255), 2)
+
+    arcLength = cv2.arcLength(contours[0], True)
+    points = cv2.approxPolyDP(contours[0], 0.02 * arcLength, True)
+    res = cv2.multiply(img, 0)
+    points = points.reshape(4, 2)
+    cv2.drawContours(res, [points], -1, (255, 255, 255), 2)
+    cv2.imshow("1",res)
+    res = transform.four_point_transform(img, points)
+
+    return res
+
+
+if __name__ == '__main__':
+    outline = getOutline("../img/bill2.jpg")
+    cv2.imshow("outline", outline)
+    cv2.waitKey()
--- ocr/tess.py
+++ ocr/tess.py
@@ -7,18 +7,37 @@
 __author__ = 'Judgement'
 
 from PIL import Image
+import numpy as np
 import cv2
 import pytesseract
+from edge import edge
 
-url = r"F:\bills\231.jpg"
-img_origin = cv2.imread(url)
-cv2.imshow('origin', img_origin)
-img_median = cv2.medianBlur(img_origin, ksize=3)
-cv2.imshow('median', img_median)
-
-img = Image.open(url)
 pytesseract.pytesseract.tesseract_cmd = r"E:\Program Files\Tesseract-OCR\tesseract.exe"
-content = pytesseract.image_to_string(img, lang='chi_sim')
-print(content)
 
-cv2.waitKey(0)
+
+def image_to_string(img_src):
+    # 得到正位图形
+    img = edge.getOutline(img_src)
+    # 灰度
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # 锐化
+    kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]], np.float32)
+    img = cv2.filter2D(img, -1, kernel=kernel)
+    # 二值化
+    ret, img = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY)
+    # 开
+    # morphology_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 1))
+    # img = cv2.morphologyEx(img, cv2.MORPH_OPEN, morphology_kernel)
+
+    img = cv2.medianBlur(img, 1)
+
+    content = pytesseract.image_to_string(img, lang='chi_sim')
+    cv2.imshow('origin', img)
+    cv2.waitKey(0)
+    return content
+
+
+if __name__ == '__main__':
+    url = r"../img/9.png"
+    print(image_to_string(url))
